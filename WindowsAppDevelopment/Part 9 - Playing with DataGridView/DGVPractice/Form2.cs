41f58896b206c1dc5974e7e3aa56b0fa74cbbb0c
bilalshahzad139 <bilal.shahzad@yahool.com>
2017-12-28 23:10:37 +0500

    Playing with DataGridView C#

---
+++ WindowsAppDevelopment/Part 9 - Playing with DataGridView/DGVPractice/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DGVPractice
+{
+    public partial class Form2 : Form
+    {
+        int _id = 0;
+        public Form2(int id)
+        {
+            _id = id;
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //DB Call, ID pass and get data
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
