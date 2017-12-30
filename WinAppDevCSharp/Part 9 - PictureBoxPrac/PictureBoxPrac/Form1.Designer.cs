b44b17a588792b405e55a9bb03d971cebeb52ef5
bilalshahzad139 <bilal.shahzad@yahool.com>
2017-12-30 18:02:12 +0500

    Playing with PictureBox

---
+++ WinAppDevCSharp/Part 9 - PictureBoxPrac/PictureBoxPrac/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace PictureBoxPrac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(253, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Choose";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(237, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(72, 48);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(63, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Hockey";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(31, 200);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(446, 125);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "ID";
+            this.Column1.HeaderText = "ID";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "Name";
+            this.Column2.HeaderText = "Name";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Edit";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Text = "Edit";
+            this.Column3.UseColumnTextForButtonValue = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(402, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 348);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewButtonColumn Column3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WinAppDevCSharp/Part 9 - PictureBoxPrac/PictureBoxPrac/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBoxPrac
+{
+    public partial class Form1 : Form
+    {
+        private int _idToEdit = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialog1.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                var filePath = openFileDialog1.FileName;
+                pictureBox1.Image = Image.FromFile(filePath);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String name = textBox1.Text.Trim();
+            String isHockey = (checkBox1.Checked == true ? "1" : "0");
+            String unqiueName = "";
+
+            if (pictureBox1.Image != null)
+            {
+                String applicationBasePath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+                String pathToSaveImage = applicationBasePath + @"\images\";
+
+                unqiueName = Guid.NewGuid().ToString() + ".jpg";
+                String imgPath = pathToSaveImage + unqiueName;
+
+                pictureBox1.Image.Save(imgPath);
+            }
+
+            String connStr = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\LearningInUrdu\PictureBoxPrac\PictureBoxPrac\Database1.mdf;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                try
+                {
+                    String query = "";
+                    conn.Open();
+                    if (_idToEdit > 0)
+                    {
+                        query = String.Format("Update dbo.Users SET name='{0}',IsHockey='{1}',UserImage='{2}' Where ID= {3}", name, isHockey, unqiueName, _idToEdit);
+                    }
+                    else
+                    {
+                        query = String.Format("INSERT INTO dbo.Users(Name,IsHockey,UserImage,CreatedOn) Values('{0}','{1}','{2}','{3}')", name, isHockey, unqiueName, DateTime.Now.ToString());
+                    }
+                    //String query = String.Format("INSERT INTO dbo.Users(Name,IsHockey,UserImage,CreatedOn) Values('{0}','{1}','{2}','{3}')", name, isHockey, unqiueName, DateTime.Now.ToString());
+                    SqlCommand command = new SqlCommand(query, conn);
+                    var recEff = command.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+
+            LoadDataInGrid();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            String applicationBasePath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            System.IO.Directory.CreateDirectory(applicationBasePath + @"\images\");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoadDataInGrid();
+        }
+
+        private void LoadDataInGrid()
+        {
+            String connStr = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\LearningInUrdu\PictureBoxPrac\PictureBoxPrac\Database1.mdf;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    String query = "Select * from dbo.Users;";
+                    SqlCommand command = new SqlCommand(query, conn);
+                    var reader = command.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+
+                    dataGridView1.DataSource = dt;
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 2) //Edit column
+            {
+                int id = (int)dataGridView1.CurrentRow.Cells[0].Value;
+                LoadData(id);
+            }
+        }
+
+        private void LoadData(int id)
+        {
+            String connStr = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\LearningInUrdu\PictureBoxPrac\PictureBoxPrac\Database1.mdf;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    String query = String.Format("Select * from dbo.Users Where ID = {0}", id);
+                    SqlCommand command = new SqlCommand(query, conn);
+                    var reader = command.ExecuteReader();
+
+                    //We are expecting one record
+                    if (reader.Read())
+                    {
+                        _idToEdit = reader.GetInt32(0);
+                        var name = reader.GetString(1);
+                        var isHockey = reader.GetBoolean(2);
+                        var imageName = reader.GetString(3);
+
+                        textBox1.Text = name;
+                        checkBox1.Checked = isHockey;
+
+                        String applicationBasePath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+
+                        String filePath = applicationBasePath + @"\images\" + imageName;
+                        pictureBox1.Image = Image.FromFile(filePath);
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+    }
+}
